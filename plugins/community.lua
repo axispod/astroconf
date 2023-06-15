68707ff0d06c31ba48d855f46c758d68a5352aa7
Axis Pod <axispod@gmail.com>
2023-06-15 21:55:58 +0500

    Initial config

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,10 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.scrolling.nvim-scrollbar" },
+  { import = "astrocommunity.bars-and-lines.feline-nvim" },
+  { import = "astrocommunity.colorscheme.github-nvim-theme" }
+  -- { import = "astrocommunity" }
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
