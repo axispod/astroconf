e5c872ed81b866020f17894d1bab03ebe628d613
Axis Pod <axispod@gmail.com>
2023-06-16 08:39:18 +0500

    Added packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,7 +5,17 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.scrolling.nvim-scrollbar" },
-  { import = "astrocommunity.colorscheme.github-nvim-theme" }
+  { import = "astrocommunity.colorscheme.github-nvim-theme" },
+  { import = "astrocommunity.pack.angular" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.yaml" },
   -- { import = "astrocommunity" }
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
